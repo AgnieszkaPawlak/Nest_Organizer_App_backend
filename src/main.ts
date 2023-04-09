904512019198d8545db5fe196ad328855fde0ebe
Agnieszka Pawlak <agnieszka.zd@interia.pl>
2023-04-10 01:36:17 +0200

    Added logger info and error level

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { WinstonModule } from 'nest-winston';
+import { transports, format } from 'winston';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new transports.File({
+          filename: `logs/info.log`,
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        new transports.File({
+          filename: `logs/error.log`,
+          level: 'error',
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        new transports.Console({
+          format: format.combine(format.cli(), format.timestamp()),
+        }),
+      ],
+    }),
+  });
   app.use(helmet());
   app.enableCors({
     origin: 'http://localhost:3000',
